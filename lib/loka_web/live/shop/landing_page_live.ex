6090fa379915d608c0c79a1ef658ec6cf126b7ea
Jonas Cosandey <jonas.cosandey@adfinis.com>
2026-03-01 12:24:23 +0100

    feat: liveview init

---
+++ lib/loka_web/live/shop/landing_page_live.ex
@@ -0,0 +1,22 @@
+defmodule LokaWeb.Shop.LandingPageLive do
+  use LokaWeb, :live_view
+
+  alias LokaWeb.Inventory
+
+  on_mount {LokaWeb.LiveUserAuth, :live_user_optional}
+
+  @impl true
+  def render(assigns) do
+    ~H"""
+    <Layouts.app current_user={@current_user} flash={@flash}>
+      hello
+    </Layouts.app>
+    """
+  end
+
+  @impl true
+  def mount(_params, _session, socket) do
+    current_user = socket.assigns.current_user
+    {:ok, socket}
+  end
+end
--- lib/loka_web/router.ex
+++ lib/loka_web/router.ex
@@ -36,6 +36,7 @@ defmodule LokaWeb.Router do
       #
       # If an authenticated user must *not* be present:
       # on_mount {LokaWeb.LiveUserAuth, :live_no_user}
+      live "/", Shop.LandingPageLive
     end
   end
 
