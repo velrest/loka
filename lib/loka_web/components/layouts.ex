164537c56cbdb4f40a78469755c7fd2a2198d7b2
Jonas Cosandey <jonas.cosandey@adfinis.com>
2026-03-01 11:26:32 +0100

    feat: navbar

--- lib/loka_web/components/layouts.ex
+++ lib/loka_web/components/layouts.ex
@@ -26,40 +26,19 @@ defmodule LokaWeb.Layouts do
 
   """
   attr :flash, :map, required: true, doc: "the map of flash messages"
+  attr :curent_user, Loka.Accounts.User, required: false
 
   attr :current_scope, :map,
     default: nil,
     doc: "the current [scope](https://hexdocs.pm/phoenix/scopes.html)"
 
   slot :inner_block, required: true
+  slot :inner_content, doc: "ash authentication uses inner_content as a slot"
 
   def app(assigns) do
     ~H"""
     <header class="navbar px-4 sm:px-6 lg:px-8">
-      <div class="flex-1">
-        <a href="/" class="flex-1 flex w-fit items-center gap-2">
-          <img src={~p"/images/logo.svg"} width="36" />
-          <span class="text-sm font-semibold">v{Application.spec(:phoenix, :vsn)}</span>
-        </a>
-      </div>
-      <div class="flex-none">
-        <ul class="flex flex-column px-1 space-x-4 items-center">
-          <li>
-            <a href="https://phoenixframework.org/" class="btn btn-ghost">Website</a>
-          </li>
-          <li>
-            <a href="https://github.com/phoenixframework/phoenix" class="btn btn-ghost">GitHub</a>
-          </li>
-          <li>
-            <.theme_toggle />
-          </li>
-          <li>
-            <a href="https://hexdocs.pm/phoenix/overview.html" class="btn btn-primary">
-              Get Started <span aria-hidden="true">&rarr;</span>
-            </a>
-          </li>
-        </ul>
-      </div>
+      <.nav_bar current_user={@current_user} />
     </header>
 
     <main class="px-4 py-20 sm:px-6 lg:px-8">
@@ -148,6 +127,85 @@ defmodule LokaWeb.Layouts do
       >
         <.icon name="hero-moon-micro" class="size-4 opacity-75 hover:opacity-100" />
       </button>
+    </div>
+    """
+  end
+
+  @doc """
+  Renders the nav bar.
+  ## Examples
+
+      <.nav_bar current_user={@current_user} />
+  """
+  attr :curent_user, Loka.Accounts.User, required: false
+
+  def nav_bar(assigns) do
+    ~H"""
+    <div class="navbar bg-base-100 shadow-sm">
+      <div class="flex-1">
+        <a class="btn btn-ghost text-xl">keraloka</a>
+      </div>
+      <div class="flex-none">
+        <input type="text" placeholder="Search" class="input input-bordered w-24 md:w-auto" />
+        <div class="dropdown dropdown-end">
+          <div tabindex="0" role="button" class="btn btn-ghost btn-circle">
+            <div class="indicator">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                class="h-5 w-5"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+              >
+                <path
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  stroke-width="2"
+                  d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z"
+                />
+              </svg>
+              <span class="badge badge-sm indicator-item">8</span>
+            </div>
+          </div>
+          <div
+            tabindex="0"
+            class="card card-compact dropdown-content bg-base-100 z-1 mt-3 w-52 shadow"
+          >
+            <div class="card-body">
+              <span class="text-lg font-bold">8 Items</span>
+              <span class="text-info">Subtotal: $999</span>
+              <div class="card-actions">
+                <button class="btn btn-primary btn-block">View cart</button>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="dropdown dropdown-end">
+          <div tabindex="0" role="button" class="btn btn-ghost btn-circle avatar">
+            <div class="w-10 rounded-full">
+              <.icon name="hero-user-circle" class="size-8" />
+            </div>
+          </div>
+          <ul
+            tabindex="-1"
+            class="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow"
+          >
+            <%= if @current_user do %>
+              <li>
+                <a class="justify-between">
+                  Profile <span class="badge">New</span>
+                </a>
+              </li>
+              <li><a>Settings</a></li>
+              <li><.theme_toggle /></li>
+              <li><a>Logout</a></li>
+            <% else %>
+              <li><.link patch={~p"/sign-in"}>{gettext("Sign In")}</.link></li>
+              <li><.link patch={~p"/register"}>{gettext("Register")}</.link></li>
+            <% end %>
+          </ul>
+        </div>
+      </div>
     </div>
     """
   end
