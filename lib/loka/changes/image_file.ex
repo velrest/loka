10b9827534c05c556f3c25a82c5ab97e2cfda82b
Jonas Cosandey <jonas.cosandey@adfinis.com>
2026-04-25 09:35:44 +0200

    fix: image_file change so io is done after action

--- lib/loka/changes/image_file.ex
+++ lib/loka/changes/image_file.ex
@@ -19,25 +19,30 @@ defmodule Loka.Changes.Image.UploadFile do
     file = Ash.Changeset.get_argument(changeset, :file)
     item_id = Ash.Changeset.get_argument(changeset, :item_id)
 
-    if is_nil(file) or is_nil(item_id), do: changeset, else: do_upload(changeset, file, item_id)
+    if is_nil(file) or is_nil(item_id) do
+      changeset
+    else
+      filename = "#{Ecto.UUID.generate()}#{Path.extname(file.source.filename)}"
+      web_path = "/uploads/items/#{item_id}/#{filename}"
+
+      changeset
+      |> Ash.Changeset.change_attribute(:path, web_path)
+      |> Ash.Changeset.change_attribute(:filename, filename)
+      |> Ash.Changeset.change_attribute(:position, length(Loka.Inventory.list_item_images!(item_id)))
+    end
   end
 
-  defp do_upload(changeset, file, item_id) do
-    dest_dir = Path.join([:code.priv_dir(:loka), "static", "uploads", "items", item_id])
-    File.mkdir_p!(dest_dir)
-    {:ok, src_path} = Ash.Type.File.path(file)
-    filename = "#{Ecto.UUID.generate()}#{Path.extname(file.source.filename)}"
-    dest = Path.join(dest_dir, filename)
-    File.cp!(src_path, dest)
+  def after_action(changeset, record, _context) do
+    file = Ash.Changeset.get_argument(changeset, :file)
 
-    [_, web_path] = String.split(dest, "/priv/static")
-
-    changeset
-    |> Ash.Changeset.change_attribute(:path, web_path)
-    |> Ash.Changeset.change_attribute(:filename, filename)
-    |> Ash.Changeset.change_attribute(
-      :position,
-      length(Loka.Inventory.list_item_images!(item_id))
-    )
+    if is_nil(file) do
+      {:ok, record}
+    else
+      dest = Path.join([:code.priv_dir(:loka), "static", String.trim_leading(record.path, "/")])
+      File.mkdir_p!(Path.dirname(dest))
+      {:ok, src_path} = Ash.Type.File.path(file)
+      File.cp!(src_path, dest)
+      {:ok, record}
+    end
   end
 end
